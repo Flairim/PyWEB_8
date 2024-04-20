de4c01cbc67d0d96af04628e5fbf934f1be0f771
Олексій Тригуб <149113900+Flairim@users.noreply.github.com>
2024-04-20 22:34:35 +0300

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -2,27 +2,32 @@ import pika
 from mongoengine import connect
 from models import Contact
 
-connect('web8', host='mongodb+srv://flairimoll:h4G2#6tAA$.s59Z@web8.jltbxwa.mongodb.net/?retryWrites=true&w=majority&appName=web8')
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-
-channel.queue_declare(queue='contacts_queue', durable=True)
-
-def send_email(contact_id):
-    print(f"Відправлено email контакту з ID: {contact_id}")
-    
-    contact = Contact.objects.get(id=contact_id)
-    contact.is_sent = True
-    contact.save()
+def connect_to_database():
+    connect('web8', host='mongodb+srv://flairimoll:h4G2#6tAA$.s59Z@web8.jltbxwa.mongodb.net/?retryWrites=true&w=majority&appName=web8')
 
 def callback(ch, method, properties, body):
-    contact_id = body.decode('utf-8')
-    send_email(contact_id)
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+    contact_id = body.decode()
+    contact = Contact.objects.get(id=contact_id)
+    print(f" [x] Отримано {contact_id}")
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='contacts_queue', on_message_callback=callback)
+    send_email_stub(contact)
 
-print('Чекаю на повідомлення. Для виходу натисніть CTRL+C')
-channel.start_consuming()
+    contact.is_sent = True
+    contact.save()
+    print(f" [x] Позначено {contact_id} як відправлено")
+
+def send_email_stub(contact):
+    print(f" [x] Надсилаємо email на {contact.email} з темою '{contact.subject}' та повідомленням '{contact.message}'")
+
+if __name__ == "__main__":
+    connect_to_database()
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='contacts_queue')
+
+    channel.basic_consume(queue='contacts_queue', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Очікування повідомлень. Для виходу натисніть CTRL+C')
+    channel.start_consuming()
